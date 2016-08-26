024f76e28f4d77481c2702f1d7c34e4120ee3a3c
Matt Gardner <matthewlgardner@gmail.com>
2016-08-26 15:17:49 -0600

    added code to be able to console log movie ids

--- app.js
+++ app.js
@@ -2,5 +2,35 @@ $(document).ready(function() {
 
     console.log('locked and loaded');
 
+    // access guidebox api
+    $('.btn').on('click', function(event) {
+        // console.log(event.target);
+        if ($('#search').val() === '') {
+            Materialize.toast("Please enter in a movie title", 3000)
+        }
+
+        var searchTerm = ($('#search').val())
+            // console.log(searchTerm);
+        $.ajax({
+            method: 'Get',
+            url: `https://api-public.guidebox.com/v1.43/US/rKdlIiwGSvS2KHovOWT8IdWJseMGPYiH/search/movie/title/${searchTerm}/fuzzy`,
+            dataType: 'json',
+            success: function(data) {
+                // console.log(data);
+                allMovies = data.results
+                for (var i = 0; i < allMovies.length; i++) {
+                    let movieList = allMovies[i]
+                    let movieIds = (movieList.id);
+                }
+
+                let movieResults = (data.results[0]);
+                let movieId = (movieResults.id);
+                // console.log(movieId);
+                // let movieId = data.id
+                // console.log(movieId);
+            }
+        })
+    })
+
 
 });
