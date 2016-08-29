a736c2af745d69128d85e52079c0f76de55f8d79
Matt Gardner <matthewlgardner@gmail.com>
2016-08-29 14:33:52 -0600

    appended all movie posters and trailers to page - sizing not fixed

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 
     console.log('locked and loaded');
 
+    let movies = []
+
+    function loadMovies() {
+
+        for (var movie of movies) {
+
+        }
+    };
+
+
     // access guidebox api
     $('.btn').on('click', function(event) {
         // console.log(event.target);
@@ -16,8 +26,11 @@ $(document).ready(function() {
             url: `https://api-public.guidebox.com/v1.43/US/rKdlIiwGSvS2KHovOWT8IdWJseMGPYiH/search/movie/title/${searchTerm}/fuzzy`,
             dataType: 'json',
             success: function(data) {
-                // console.log(data);
-                allMovies = data.results
+                // console.log('ajax #1 data returned');
+                movies = []
+                    // console.log(data);
+                let allMovies = data.results
+                    // loop thru all movies and grab id
                 for (var i = 0; i < allMovies.length; i++) {
                     let movieList = allMovies[i]
                     let movieIds = (movieList.id);
@@ -27,17 +40,33 @@ $(document).ready(function() {
                         url: `https://api-public.guidebox.com/v1.43/US/rKdlIiwGSvS2KHovOWT8IdWJseMGPYiH/movie/${movieIds}`,
                         dataType: 'json',
                         success: function(newData) {
-                            console.log(newData);
+                            // console.log('ajax #2 returned');
+                            // console.log(newData);
+                            movies.push(newData)
+                                // console.log(movies);
+                                // appends movie poster to page
                             let moviePoster = newData.poster_240x342
+                            let movieDiv = $('<div class="col m4 l12">')
+                            $('<img>', {
+                                src: moviePoster,
+                                alt: `${newData.title}`
+                            }).appendTo(movieDiv)
+                            $('.poster').append(movieDiv)
                                 // console.log(moviePoster);
-                            $('img').attr({
-                                src: moviePoster
-                            })
-                            let purchaseSources = (newData.purchase_web_sources);
-                            // console.log(purchaseSources);
-                            for (var i = 0; i < purchaseSources.length; i++) {
-                                // console.log(purchaseSources[i])
-                            }
+
+                            // movie trailer
+                            let trailer = newData.trailers.web[0].embed
+                            console.log(trailer);
+                            let iframe = $('<iframe>')
+                            $('<iframe>', {
+                                src: trailer,
+                            }).appendTo(movieDiv)
+
+                            // let purchaseSources = (newData.purchase_web_sources);
+                            // // console.log(purchaseSources);
+                            // for (var i = 0; i < purchaseSources.length; i++) {
+                            // console.log(purchaseSources[i])
+                            // }
                         },
                         error: function(err) {
                             console.log('newData ERROR msg:', err);
@@ -50,6 +79,5 @@ $(document).ready(function() {
             }
         })
     })
-
 
 });
---
+++ eslintrc.js
@@ -0,0 +1,10 @@
+module.exports - {
+    extends: [
+        'ryansobol/browser',
+        'ryansobol/es6'
+    ]
+    rules: {
+        "quotes": "off"
+            // "semi": "off"
+    }
+}
