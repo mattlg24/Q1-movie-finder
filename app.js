8d460b7e01f611094ab9608a7b0f12c475508d5d
Matt Gardner <matthewlgardner@gmail.com>
2016-08-30 12:04:57 -0600

    appended and fixed layout of search results

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     let movies = []
 
-    function loadMovies() {
+    function renderMovies() {
 
         for (var movie of movies) {
 
@@ -14,7 +14,8 @@ $(document).ready(function() {
 
     // access guidebox api
     $('.btn').on('click', function(event) {
-        // console.log(event.target);
+        $('.media').empty()
+            // console.log(event.target);
         if ($('#search').val() === '') {
             Materialize.toast("Please enter in a movie title", 3000)
         }
@@ -40,44 +41,65 @@ $(document).ready(function() {
                         url: `https://api-public.guidebox.com/v1.43/US/rKdlIiwGSvS2KHovOWT8IdWJseMGPYiH/movie/${movieIds}`,
                         dataType: 'json',
                         success: function(newData) {
-                            // console.log('ajax #2 returned');
-                            // console.log(newData);
-                            movies.push(newData)
-                                // console.log(movies);
-                                // appends movie poster to page
+                            console.log(newData);
                             let moviePoster = newData.poster_240x342
-                            let movieDiv = $('<div class="col m4 l12">')
-                            $('<img>', {
-                                src: moviePoster,
-                                alt: `${newData.title}`
-                            }).appendTo(movieDiv)
-                            $('.poster').append(movieDiv)
-                                // console.log(moviePoster);
-
-                            // movie trailer
+                                // movie trailer
                             let trailer = newData.trailers.web[0].embed
-                            let iframe = $('<iframe>')
-                            $('<iframe>', {
-                                src: trailer,
-                            }).appendTo(movieDiv)
-
-
-                            // watch sources
-                            let sourceLinks;
+                                // watch sources
+                            var sourceLinks;
                             let purchaseSources = (newData.purchase_web_sources);
+                            let iTunes = purchaseSources[0]
+                            let source2 = purchaseSources[1]
+                            let source3 = purchaseSources[2]
+                            let source4 = purchaseSources[3]
+                            let source5 = purchaseSources[4]
+                            console.log('source1', source1);
                             // loops thru all watch sources
-                            for (var i = 0; i < purchaseSources.length; i++) {
-                                let sources = purchaseSources[i]
-                                    // console.log(newData.title);
-                                sourceLinks = (sources.link);
-                                // console.log(sourceLinks);
-                                // appends links to page
-                                $('<a>', {
-                                    href: sourceLinks,
-                                    text: 'Watch Here'
-                                }).appendTo(movieDiv)
-                            }
-
+                            // for (var i = 0; i < purchaseSources.length; i++) {
+                            //     let sources = purchaseSources[i]
+                            //     console.log(newData);
+                            //     sourceLinks = sources.link;
+                            //     // console.log('sourcelinks', sourceLinks);
+                            // }
+                            // console.log('sourcelinks=========================', sourceLinks);
+                            // console.log('sourcelinks===============length==========', sourceLinks.length);
+                            // end of tempory commented section
+                            let contentDiv = `<div id="poster" class="col s12 m4 l3">
+                                <div class="card">
+                                    <div class="card-image">
+                                        <img src="${moviePoster}">
+                                    </div>
+                                    <div class="card-action">
+                                        <a href="#">This is a link</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="trailer col s12 m8 l9">
+                                <iframe width="100%" height="362px" src="${trailer}">
+                                </iframe>
+                            </div>
+                            <div class="row">
+                                <div class="col l1">
+                                    <a href="${source1.link}"><img src="${source1.link}"></a>
+                                </div>
+                                <div class="col l1">
+                                    <a href="${sourceLinks}"><img src="http://www.fillmurray.com/50/50"></a>
+                                </div>
+                                <div class="col l1">
+                                    <a href="${sourceLinks}"><img src="http://www.fillmurray.com/50/50"></a>
+                                </div>
+                                <div class="col l1">
+                                    <a href="${sourceLinks}"><img src="http://www.fillmurray.com/50/50"></a>
+                                </div>
+                                <div class="col l1">
+                                    <a href="${sourceLinks}"><img src="http://www.fillmurray.com/50/50"></a>
+                                </div>
+                                <div class="col l1">
+                                    <a href="${sourceLinks}"><img src="http://www.fillmurray.com/50/50"></a>
+                                </div>
+                            </div>`
+                                // append content to page
+                            $('.media').append(contentDiv)
                         },
                         error: function(err) {
                             console.log('newData ERROR msg:', err);
@@ -91,4 +113,10 @@ $(document).ready(function() {
         })
     })
 
-});
+    //scroll to top when button clicked
+    $("#toTop").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".container").offset().top
+        }, 800);
+    });
+});;
